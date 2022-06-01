ef3fb07b2d2565d74acfa44a88dbcf63764fbb52
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2022-06-01 14:51:48 +0200

    Modularity&custom.scss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import "./custom.scss";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
      <h1>Hello World</h1>
     </div>
   );
